ee0a96d0c9d9829f5dbe263a53fd4ae9af7a36b5
swordboyly <15776503584@163.com>
2018-10-10 16:55:36 +0800

    支持公众号自定义文本回复

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask import request
@@ -12,22 +12,22 @@ def index():
         echostr = request.args.get('echostr')
         return  echostr
     else:
-        data=request.get_data()
-        xml=ET.fromstring(data)
-        ToUserName=xml.findtext(".//ToUserName")
-        FromUserName=xml.findtext(".//FromUserName")
-        CreateTime=xml.findtext(".//CreateTime")
-        MsgType=xml.findtext(".//MsgType")
-        Content=xml.findtext(".//Content")
-        if MsgType=='text':
-            if Content=='hi':
-                return render_template('index.html',toUser=FromUserName,fromUser=ToUserName,CreateTime=CreateTime,content=u'你好啊！！！')
+        data = request.get_data()
+        xml = ET.fromstring(data)
+        ToUserName = xml.findtext(".//ToUserName")
+        FromUserName = xml.findtext(".//FromUserName")
+        CreateTime = xml.findtext(".//CreateTime")
+        MsgType = xml.findtext(".//MsgType")
+        Content = xml.findtext(".//Content")
+        if MsgType == 'text':
+            if Content == 'hi':
+                return render_template('index.html',toUser = FromUserName,fromUser = ToUserName,CreateTime = CreateTime,content = u'你好啊！！！')
             else:
-                return render_template('index.html',toUser=FromUserName,fromUser=ToUserName,CreateTime=CreateTime,content=u'很高兴见到你！！！')
+                return render_template('index.html',toUser = FromUserName,fromUser = ToUserName,CreateTime = CreateTime,content = u'很高兴见到你！！！')
         else:
-            return render_template('index.html', toUser=FromUserName, fromUser=ToUserName, CreateTime=CreateTime,content='nice to meet you!!!')
+            return render_template('index.html', toUser = FromUserName, fromUser = ToUserName, CreateTime = CreateTime,content = 'nice to meet you!!!')
 
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
